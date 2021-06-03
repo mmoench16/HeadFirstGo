e4e59a9ecc905c81fdcaea0a4761f3a702af83de
mmoench16 <m.moench16@gmail.com>
2021-06-03 20:34:20 +0200

    just a tiny wee bit tonight..too tired

--- Chapter7/count/main.go
+++ Chapter7/count/main.go
@@ -13,22 +13,29 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	var names []string
-	var counts []int
+	// Complicated Way
+	// var names []string
+	// var counts []int
+	// for _, line := range lines {
+	// 	matched := false
+	// 	for i, name := range names {
+	// 		if name == line {
+	// 			counts[i]++
+	// 			matched = true
+	// 		}
+	// 	}
+	// 	if matched == false {
+	// 		names = append(names, line)
+	// 		counts = append(counts, 1)
+	// 	}
+	// }
+	// for i, name := range names {
+	// 	fmt.Printf("%s: %d\n", name, counts[i])
+	// }
+	// Easier way (using maps)
+	counts := make(map[string]int)
 	for _, line := range lines {
-		matched := false
-		for i, name := range names {
-			if name == line {
-				counts[i]++
-				matched = true
-			}
-		}
-		if matched == false {
-			names = append(names, line)
-			counts = append(counts, 1)
-		}
+		counts[line]++
 	}
-	for i, name := range names {
-		fmt.Printf("%s: %d\n", name, counts[i])
-	}
+	fmt.Println(counts)
 }
